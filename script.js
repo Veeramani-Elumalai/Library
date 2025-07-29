f070de44dd290804f224608c3acecc3cf3a79c39
Veeramani Elumalai <veeramanielumalai2004@gmail.com>
2025-07-29 21:43:17 +0500

    Function created for Display the books

--- script.js
+++ script.js
@@ -25,8 +25,35 @@ formData.addEventListener('submit',function(event){
     console.log(myLibrary);
 })
 
-function addBookToLibrary(){
-    
+//Functionality to create new book image
+
+function displayBooks(){
+    const container = document.getElementById('book_list');
+    container.innerHTML = '';
+
+    myLibrary.forEach(book =>{
+        const bookCard = document.createElement('div'); // One card per book
+        bookCard.classList.add('book');
+
+        const disImage = document.createElement("img");
+        disImage.src = "imgs/ATOMIC_HABITS.png";
+
+        const disTitle = document.createElement('h4');
+        disTitle.textContent = book.title;
+
+        const disAuthor = document.createElement('p');
+        disAuthor.textContent = book.author;
+
+        const disPages = document.createElement('p');
+        disPages.textContent = `${book.pages} pages`;
+
+        bookCard.appendChild(disImage);
+        bookCard.appendChild(disTitle);
+        bookCard.appendChild(disAuthor);
+        bookCard.appendChild(disPages);
+
+        container.appendChild(bookCard);
+    });
 }
 
 function userInput(){
