2b07155f3bbdf6cfc65ca0aa494bab3accb7d3bf
Veeramani Elumalai <veeramanielumalai2004@gmail.com>
2025-07-29 20:18:15 +0500

    Added functionality to the dialog box

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 const myLibrary = [];
+const addBook = document.getElementById('add_book');
+const bookInput = document.getElementById('book_input');
 
 function book(title,author,pages,activity){
     this.title = title;
@@ -8,6 +10,12 @@ function book(title,author,pages,activity){
 }
 
 function addBookToLibrary(){
+   // addBook.addEventListener('click',function(){
+   //     bookInput.showModal;            
+  //  });
     
 }
 
+function userInput(){
+    bookInput.showModal();
+}
